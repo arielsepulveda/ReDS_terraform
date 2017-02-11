56c06fd8e482a950da3941056691a8c4cb3116e9
cascompany <cascompany@gmail.com>
2017-02-11 01:46:42 +0100

    module changes

--- modules/iamrole/main.tf
+++ modules/iamrole/main.tf
@@ -13,19 +13,6 @@ variable "unique_name" {
 resource "aws_iam_role" "reds-role-lambdarole" {
     name               = "reds-role-lambdarole-${var.unique_name}"
     path               = "/"
-    assume_role_policy = <<POLICY
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+    assume_role_policy = "${file("lambdarole.json")}"
 }
 output "aws_iam_role_id" { value = "${aws_iam_role.reds-role-lambdarole.id}" }
